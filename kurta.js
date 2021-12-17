176d3dfc95f6aa60997cd8a5f6bf0e53e8ff7ae1
Niks14041 <nikhil14041@gmail.com>
2021-12-17 09:06:46 +0530

    updated

--- kurta.js
+++ kurta.js
@@ -1,186 +1,11 @@
-// var data = [{
-//     img: "https://pantaloons.imgix.net/img/app/product/4/459126-3158237.jpg?w=262&q=75",
-//     brand: "INDUS ROUTE",
-//     description: "Solid Off White Kurta",
-//     price: 480,
-//     discount: "20% OFF",
-//     preprice: "₹ 599"
-// },
-// {
-//     img: "https://pantaloons.imgix.net/img/app/product/6/644808-6453357.jpg?w=262&q=75",
-//     brand: "INDUS ROUTE",
-//     description: "Red Striped Short Kurta",
-//     price: 640,
-//     discount: "20% OFF",
-//     preprice: "₹ 799"
-// },
-// {
-//     img: "https://pantaloons.imgix.net/img/app/product/6/609051-5751034.jpg?w=262&q=75",
-//     brand: "INDUS ROUTE",
-//     description: "Olive Solid Kurta",
-//     price: 599,
-// },
-// {
-//     img: "https://pantaloons.imgix.net/img/app/product/4/439012-2844007.jpg?w=262&q=75",
-//     brand: "INDUS ROUTE",
-//     description: "Embroidered Maroon Kurta",
-//     price: 975,
-//     discount: "25% OFF",
-//     preprice: "₹ 1299"
-// },
-// {
-//     img: "https://pantaloons.imgix.net/img/app/product/6/644805-6456762.jpg?w=262&q=75",
-//     brand: "INDUS ROUTE",
-//     description: "Lime Patterned Short Kurta",
-//     price: 640,
-//     discount: "20% OFF",
-//     preprice: "₹ 799"
-// },
-// {
-//     img: "https://pantaloons.imgix.net/img/app/product/4/458845-3156566.jpg?w=262&q=75",
-//     brand: "INDUS ROUTE",
-//     description: "Solid Charcoal Kurta",
-//     price: 560,
-//     discount: "20% OFF",
-//     preprice: "₹ 699",
-// },
-// {
-//     img: "https://pantaloons.imgix.net/img/app/product/6/634061-6695767.jpg?w=262&q=75",
-//     brand: "INDUS ROUTE",
-//     description: "Blue Short Kurta",
-//     price: 599,
-// },
-// {
-//     img: "https://pantaloons.imgix.net/img/app/product/5/558245-4626286.jpg?w=262&q=75",
-//     brand: "INDUS ROUTE",
-//     description: "Indigo Embroidered Kurta",
-//     price: 840,
-//     discount: "30% OFF",
-//     preprice: "₹ 1199",
-// },
-// {
-//     img: "https://pantaloons.imgix.net/img/app/product/6/643192-6427044.jpg?w=262&q=75",
-//     brand: "INDUS ROUTE",
-//     description: "Teal Patterned Kurta",
-//     price: 899,
-// },
-// {
-//     img: "https://pantaloons.imgix.net/img/app/product/6/643189-6426770.jpg?w=262&q=75",
-//     brand: "INDUS ROUTE",
-//     description: "Yellow Patterned Short Kurta",
-//     price:750,
-//     discount: "25% OFF",
-//     preprice: "₹ 999",
-// },
-// {
-//     img: "https://pantaloons.imgix.net/img/app/product/4/461199-3191557.jpg?w=262&q=75",
-//     brand: "INDUS ROUTE",
-//     description: "Solid Rust Kurta",
-//     price: 640,
-//     discount: "20% OFF",
-//     preprice: "₹ 799",
-// },
-// {
-//     img: "https://pantaloons.imgix.net/img/app/product/6/645701-6480611.jpg?w=262&q=75",
-//     brand: "INDUS ROUTE",
-//     description: "Off White Patterned Solid Kurta ",
-//     price: 2299,
 
-// },
-// {
-//     img: "https://pantaloons.imgix.net/img/app/product/4/468621-3308571.jpg?w=262&q=75",
-//     brand: "INDUS ROUTE",
-//     description: "Printed Blue Kurta",
-//     price: 560,
-//     discount: "20% OFF",
-//     preprice: "₹ 699",
-// },
-// {
-//     img: "https://pantaloons.imgix.net/img/app/product/6/644794-6462410.jpg?w=262&q=75",
-//     brand: "INDUS ROUTE",
-//     description: "Olive Solid Short Kurta",
-//     price: 790,
-
-// },
-// {
-//     img: "https://pantaloons.imgix.net/img/app/product/6/640353-6379701.jpg?w=262&q=75",
-//     brand: "INDUS ROUTE",
-//     description: "Off-White Solid Kurta",
-//     price: 1299,
-// },
-// {
-//     img: "https://pantaloons.imgix.net/img/app/product/4/479311-3472900.jpg?w=262&q=75",
-//     brand: "PETER ENGLAND",
-//     description: "Teal Solid Kurta",
-//     price: 1299,
-// },
-// {
-//     img: "https://pantaloons.imgix.net/img/app/product/6/623764-6104118.jpg?w=262&q=75",
-//     brand: "PETER ENGLAND",
-//     description: "Indigo Short Kurta",
-//     price: 1199,
-
-// },
-// {
-//     img: "https://pantaloons.imgix.net/img/app/product/6/644810-6456783.jpg?w=262&q=75",
-//     brand: "PETER ENGLAND",
-//     description: "Maroon Solid Short Kurta",
-//     price: 960,
-// },
-// {
-//     img: "https://pantaloons.imgix.net/img/app/product/6/643194-6427073.jpg?w=262&q=75",
-//     brand: "PETER ENGLAND",
-//     description: "Rust Patterned Kurta",
-//     price: 1590,
-
-// },
-// {
-//     img: "https://pantaloons.imgix.net/img/app/product/6/645698-6480602.jpg?w=262&q=75",
-//     brand: "PETER ENGLAND",
-//     description: "Gold Kurta Waistcoat Set",
-//     preprice: 2299,
-// },
-// {
-//     img: "https://pantaloons.imgix.net/img/app/product/6/647384-6508487.jpg?w=262&q=75",
-//     brand: "PETER ENGLAND",
-//     description: "Red Embroidered Kurta",
-//     price:  1699,
-
-// },
-// {
-//     img: "https://pantaloons.imgix.net/img/app/product/6/634064-6695788.jpg?w=262&q=75",
-//     brand: "PETER ENGLAND",
-//     description: "Mustard Kurta",
-//     price: 1499,
-
-// },
-// {
-//     img: "https://pantaloons.imgix.net/img/app/product/6/614349-6102781.jpg?w=262&q=75",
-//     brand: "PETER ENGLAND",
-//     description: "Gold Long Kurta",
-//     price:1299,
-
-// },
-// {
-//     img: "https://pantaloons.imgix.net/img/app/product/5/521591-3923990.jpg?w=262&q=75",
-//     brand: "PEOPLE",
-//     description: "Red solid Kurta",
-//     price: 840,
-//     discount: "30% OFF",
-//     preprice: "₹ 1199",
-
-// }];
+ var data=JSON.parse(localStorage.getItem("data")) || [];
+ displayitem(data);
 
 
-
-// localStorage.setItem("data2",JSON.stringify(data))
- var data=JSON.parse(localStorage.getItem("data")) || []
-console.log(data)
- displayitem(data)
-
-
-function displayitem(data){
-    document.querySelector(".main").innerHTML=""
+function displayitem(data)
+{
+    document.querySelector(".main").innerHTML="";
 data.map(function (elem) {
     var div = document.createElement("div");
     div.setAttribute("class", "product-box");
@@ -228,15 +53,13 @@ data.map(function (elem) {
 
     div.append(child_div)
 
-    document.querySelector(".main").append(div)
+    document.querySelector(".main").append(div);
 });
 
 }
-
-
  function numsort(){
      var selected = document.querySelector("#sort").value
-    console.log(selected)
+
     if(selected=="low"){
         data.sort(function(a,b){
             return Number(a.price)-Number(b.price)
@@ -261,26 +84,22 @@ data.map(function (elem) {
 
   
             var selected=document.querySelector("#filterbrand").value
-            console.log(selected)
-           
-
             var brand =data.filter(function(el){
                 // return selected ==el.cat;
                 return selected == el.brand
             })
-                 console.log(brand)
             displayitem(brand)
 }
 
 function filterdiscount(){
     var selected=document.querySelector("#filterdiscount").value
-    console.log(selected)
+
 
 
     var discount=data.filter(function(el){
         return selected==el.discount
-        console.log(el.discount)
     })
     
     displayitem(discount)
 }
+ 
